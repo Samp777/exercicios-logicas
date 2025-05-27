036962dad90a172c885bc79c7f081e51410e3a8c
Samp777 <leonardoxsampaio@gmail.com>
2025-05-27 17:08:17 -0300

    unidades,js
    codiguim do free fire

---
+++ unidades.js
@@ -0,0 +1,10 @@
+// ler a idade do usuario
+// se idade 18 ou maior, dizer que é maior
+//senão,diz que é menor
+
+let idade = prompt ("qual sua idade?")
+if(idade >= 18){
+    alert("voce é maior")
+}else {
+    alert ("você é menor de idade.")
+}
